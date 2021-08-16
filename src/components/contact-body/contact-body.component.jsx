9ab9eb96bf7ee244e83835c5c425004246f804f1
Chanwoo KIM <chanwoo.kim@outlook.fr>
2021-08-16 16:44:50 +0200

    feat(contact page): add mail and linkedIn buttons

--- src/components/contact-body/contact-body.component.jsx
+++ src/components/contact-body/contact-body.component.jsx
@@ -2,6 +2,10 @@ import { React } from 'react';
 
 import './contact-body.styles.scss';
 
+// import images
+import imgMail from './../../assets/mail.svg';
+import imgLinkedin from './../../assets/linkedin.svg';
+
 const myContact = {
   email: {
     email: 'chanwoo.kim@outlook.fr',
@@ -27,13 +31,21 @@ const ContactBody = () => {
             </a>
           </span>
         </div>
-        <div className="info">
-          <span>LinkedIn : </span>
-          <span>
-            <a target="_blank" rel="noreferrer" href={myContact.linkedIn}>
-              {myContact.linkedIn}
-            </a>
-          </span>
+      </div>
+      <div className="icon-container">
+        <div className="icon">
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href={myContact.email.mailto + myContact.email.email}
+          >
+            <img src={imgMail} alt="logo mail" />
+          </a>
+        </div>
+        <div className="icon">
+          <a target="_blank" rel="noreferrer" href={myContact.linkedIn}>
+            <img src={imgLinkedin} alt="logo linkedin" />
+          </a>
         </div>
       </div>
     </div>
